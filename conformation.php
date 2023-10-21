05c2e89ba322d9018266b60cac89ecd1eb90dbcb
Toralf Richter <73883286+richtertoralf@users.noreply.github.com>
2023-10-22 00:22:36 +0200

    Update conformation.php

--- conformation.php
+++ conformation.php
@@ -1,20 +1,41 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Ihre Titel hier</title>
+  <link rel="stylesheet" href="styles.css"> <!-- Hier wird die CSS-Datei eingebunden -->
+</head>
+
+<body>
+
+  <?php
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Formulardaten erhalten
     $spNr = $_POST["regEntry"][0]["SpNr"];
     $surname = $_POST["regEntry"][0]["surname"];
     $forename = $_POST["regEntry"][0]["forename"];
     $sex = $_POST["regEntry"][0]["sex"];
+    $yearofbirth = $_POST["regEntry"][0]["yearofbirth"];
     $group = $_POST["regEntry"][0]["group"];
     $email = $_POST["single_email"];
-    $message = $_POST["single_message"];
+
+    // Spaltenköpfe für die CSV-Datei
+    $headers = ["Startpassnummer", "Nachname", "Vorname", "Geschlecht", "Geburtsjahr", "Gruppe", "E-Mail"];
 
     // Daten in CSV-Datei speichern
-    $data = [$spNr, $surname, $forename, $sex, $group, $email, $message];
+    $data = [$spNr, $surname, $forename, $sex, $yearofbirth, $group, $email];
     $csvFile = fopen("data.csv", "a"); // öffne die CSV-Datei im Anhänge-Modus
-    fputcsv($csvFile, $data); // schreibe die Daten in die CSV-Datei
-    fclose($csvFile); // schließe die CSV-Datei
 
+    if (filesize("data.csv") == 0) {
+      // Wenn die Datei leer ist, schreibe die Spaltenköpfe
+      fputcsv($csvFile, $headers);
+    }
+
+    fputcsv($csvFile, $data); // schreibe die Daten in die CSV-Datei
+    // fwrite($csvFile, PHP_EOL); // füge einen Zeilenumbruch hinzu
+    fclose($csvFile); // schließe die CSV-Datei
     // Daten auf der Webseite anzeigen
     echo "<h2>Einzelanmeldung erfolgreich eingegangen:</h2>";
     echo "<ul>";
@@ -22,11 +43,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "<li><strong>Nachname:</strong> $surname</li>";
     echo "<li><strong>Vorname:</strong> $forename</li>";
     echo "<li><strong>Geschlecht:</strong> $sex</li>";
+    echo "<li><strong>Geburtsjahr:</strong> $yearofbirth</li>";
     echo "<li><strong>Gruppe:</strong> $group</li>";
     echo "<li><strong>E-Mail:</strong> $email</li>";
-    echo "<li><strong>Anmerkung:</strong> $message</li>";
     echo "</ul>";
-} else {
+  } else {
     echo "Ungültige Anfrage";
-}
-?>
+  }
+  ?>
+
+  <nav>
+    <form action="index.php" method="get">
+      <button type="submit">Weitere Anmeldung</button>
+    </form>
+    <form action="participants.php" method="get">
+      <button type="submit">Meldeliste anzeigen</button>
+    </form>
+    <form action="unsubscribe.php" method="get">
+      <!-- Ersetzen Sie "abmeldeseite.php" durch die URL Ihrer Abmeldeseite -->
+      <button type="submit">Anmeldung beenden</button>
+    </form>
+  </nav>
+
+</body>
+
+</html>
