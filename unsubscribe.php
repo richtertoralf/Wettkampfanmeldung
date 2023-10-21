6d42cd611e8970251f677c0ccffce75984fbab8d
Toralf Richter <73883286+richtertoralf@users.noreply.github.com>
2023-10-22 00:29:30 +0200

    Create unsubscribe.php

---
+++ unsubscribe.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Danke</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <h1>Auf Wiedersehen</h1>
+    <p>Danke für deine Anmeldung.</p>
+
+</body>
+
+</html>
