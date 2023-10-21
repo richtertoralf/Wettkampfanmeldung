0034e82d5e0ffc991bd8fefa3a9548e034f90907
Toralf Richter <73883286+richtertoralf@users.noreply.github.com>
2023-10-21 21:27:48 +0200

    Create participants.php

---
+++ participants.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <!-- Hier kommen die Meta-Tags und Stylesheets -->
+    <title>Meldeliste</title>
+</head>
+<body>
+    <h1>Meldeliste</h1>
+    <!-- Hier zeigst du die Teilnehmer aus der data.csv an -->
+    <!-- Hier kommt ein Link, um die Meldeliste herunterzuladen -->
+    <a href="download.php">Meldeliste als CSV herunterladen</a>
+</body>
+</html>
