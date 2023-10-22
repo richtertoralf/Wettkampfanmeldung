e24bef67fb3cafae6e65d7ed425ecec334f20dd0
Toralf Richter <73883286+richtertoralf@users.noreply.github.com>
2023-10-23 00:06:37 +0200

    Update participants.php

--- participants.php
+++ participants.php
@@ -9,60 +9,54 @@
 </head>
 
 <body>
-    <h1>Meldeliste</h1>
+    <div class="container_participants">
+        <h2>Meldeliste</h2>
 
-    <?php
-    // CSV-Datei öffnen und Inhalte lesen
-    $csvFile = fopen("data.csv", "r");
-    if ($csvFile !== FALSE) {
-        // Tabelle für die Anzeige erstellen
-        echo '<table border="1">
-                <tr>
-                    <th>Startpassnummer</th>
-                    <th>Nachname</th>
-                    <th>Vorname</th>
-                    <th>Geschlecht</th>
-                    <th>Geburtsjahr</th>
-                    <th>Gruppe</th>
-                    <th>E-Mail</th>
-                </tr>';
+        <?php
+        // CSV-Datei öffnen und Inhalte lesen
+        $csvFile = fopen("data.csv", "r");
 
-        // Daten aus der CSV-Datei lesen und in die Tabelle einfügen
-        while (($data = fgetcsv($csvFile, 1000, ",")) !== FALSE) {
+        if ($csvFile !== FALSE) {
+            // Tabelle für die Anzeige erstellen
+            echo '<table border="1">';
+
+            // Spaltenköpfe aus der ersten Zeile der CSV-Datei lesen und in die Tabelle einfügen
+            $headers = fgetcsv($csvFile, 1000, ",");
             echo '<tr>';
-            foreach ($data as $value) {
-                echo '<td>' . htmlspecialchars($value) . '</td>';
+            foreach ($headers as $header) {
+                echo '<th>' . htmlspecialchars($header) . '</th>';
             }
             echo '</tr>';
+
+            // Daten aus der CSV-Datei lesen und in die Tabelle einfügen
+            $rowCounter = 0;
+            while (($data = fgetcsv($csvFile, 1000, ",")) !== FALSE && $rowCounter < 2000) {
+                echo '<tr>';
+                foreach ($data as $value) {
+                    echo '<td>' . htmlspecialchars($value) . '</td>';
+                }
+                echo '</tr>';
+                $rowCounter++;
+            }
+
+            // Tabelle schließen
+            echo '</table>';
+
+            // CSV-Datei schließen
+            fclose($csvFile);
+        } else {
+            echo 'Fehler beim Öffnen der CSV-Datei.';
         }
+        ?>
 
-        // Tabelle schließen
-        echo '</table>';
+        <!-- Hier kommt ein Link, um die Meldeliste herunterzuladen -->
+        <a href="download.php" class="download-link">Meldeliste als CSV herunterladen</a>
 
-        // CSV-Datei schließen
-        fclose($csvFile);
-    } else {
-        echo 'Fehler beim Öffnen der CSV-Datei.';
-    }
-
-    ?>
-
-    <!-- Hier kommt ein Link, um die Meldeliste herunterzuladen -->
-    <a href="download.php">Meldeliste als CSV herunterladen</a>
-
-    <nav>
-        <form action="index.php" method="get">
-            <button type="submit">Weitere Anmeldung</button>
-        </form>
-        <form action="participants.php" method="get">
-            <button type="submit">Meldeliste anzeigen</button>
-        </form>
-        <form action="unsubscribe.php" method="get">
-            <!-- Ersetzen Sie "abmeldeseite.php" durch die URL Ihrer Abmeldeseite -->
-            <button type="submit">Anmeldung beenden</button>
-        </form>
-    </nav>
-
+        <?php
+        // Navigation einfügen
+        include('navigation.php');
+        ?>
+    </div>
 </body>
 
 </html>
