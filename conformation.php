cb367fe7e5fa35b04ff298bdd2dcf263b5d873f3
Toralf Richter <73883286+richtertoralf@users.noreply.github.com>
2023-10-23 00:04:31 +0200

    Update conformation.php

--- conformation.php
+++ conformation.php
@@ -10,61 +10,59 @@
 
 <body>
 
-  <?php
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Formulardaten erhalten
-    $spNr = $_POST["regEntry"][0]["SpNr"];
-    $surname = $_POST["regEntry"][0]["surname"];
-    $forename = $_POST["regEntry"][0]["forename"];
-    $sex = $_POST["regEntry"][0]["sex"];
-    $yearofbirth = $_POST["regEntry"][0]["yearofbirth"];
-    $group = $_POST["regEntry"][0]["group"];
-    $email = $_POST["single_email"];
+  <div class="container_registration">
 
-    // Spaltenköpfe für die CSV-Datei
-    $headers = ["Startpassnummer", "Nachname", "Vorname", "Geschlecht", "Geburtsjahr", "Gruppe", "E-Mail"];
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      // Formulardaten erhalten
+      $spNr = $_POST["regEntry"][0]["SpNr"];
+      $surname = $_POST["regEntry"][0]["surname"];
+      $forename = $_POST["regEntry"][0]["forename"];
+      $sex = $_POST["regEntry"][0]["sex"];
+      $yearofbirth = $_POST["regEntry"][0]["yearofbirth"];
+      $club = $_POST["regEntry"][0]["club"];
+      $group = $_POST["regEntry"][0]["group"];
+      $email = $_POST["single_email"];
 
-    // Daten in CSV-Datei speichern
-    $data = [$spNr, $surname, $forename, $sex, $yearofbirth, $group, $email];
-    $csvFile = fopen("data.csv", "a"); // öffne die CSV-Datei im Anhänge-Modus
+      // Spaltenköpfe für die CSV-Datei
+      $headers = ["Startpassnummer", "Nachname", "Vorname", "Geschlecht", "Geburtsjahr", "Verein", "Gruppe", "E-Mail"];
 
-    if (filesize("data.csv") == 0) {
-      // Wenn die Datei leer ist, schreibe die Spaltenköpfe
-      fputcsv($csvFile, $headers);
+      // Daten in CSV-Datei speichern
+      $data = [$spNr, $surname, $forename, $sex, $yearofbirth, $club, $group, $email];
+      $csvFile = fopen("data.csv", "a"); // öffne die CSV-Datei im Anhänge-Modus
+
+      if (filesize("data.csv") == 0) {
+        // Wenn die Datei leer ist, schreibe die Spaltenköpfe
+        fputcsv($csvFile, $headers);
+      }
+
+      fputcsv($csvFile, $data); // schreibe die Daten in die CSV-Datei
+      // fwrite($csvFile, PHP_EOL); // füge einen Zeilenumbruch hinzu
+      fclose($csvFile); // schließe die CSV-Datei
+
+      // Daten auf der Webseite anzeigen
+      echo "<h2>Einzelanmeldung erfolgreich eingegangen:</h2>";
+      echo "<ul>";
+      echo "<li><strong>Startpassnummer:</strong> $spNr</li>";
+      echo "<li><strong>Nachname:</strong> $surname</li>";
+      echo "<li><strong>Vorname:</strong> $forename</li>";
+      echo "<li><strong>Geschlecht:</strong> $sex</li>";
+      echo "<li><strong>Geburtsjahr:</strong> $yearofbirth</li>";
+      echo "<li><strong>Verein:</strong> $club</li>";
+      echo "<li><strong>Gruppe:</strong> $group</li>";
+      echo "<li><strong>E-Mail:</strong> $email</li>";
+      echo "</ul>";
+    } else {
+      echo "Ungültige Anfrage";
     }
+    ?>
 
-    fputcsv($csvFile, $data); // schreibe die Daten in die CSV-Datei
-    // fwrite($csvFile, PHP_EOL); // füge einen Zeilenumbruch hinzu
-    fclose($csvFile); // schließe die CSV-Datei
-    // Daten auf der Webseite anzeigen
-    echo "<h2>Einzelanmeldung erfolgreich eingegangen:</h2>";
-    echo "<ul>";
-    echo "<li><strong>Startpassnummer:</strong> $spNr</li>";
-    echo "<li><strong>Nachname:</strong> $surname</li>";
-    echo "<li><strong>Vorname:</strong> $forename</li>";
-    echo "<li><strong>Geschlecht:</strong> $sex</li>";
-    echo "<li><strong>Geburtsjahr:</strong> $yearofbirth</li>";
-    echo "<li><strong>Gruppe:</strong> $group</li>";
-    echo "<li><strong>E-Mail:</strong> $email</li>";
-    echo "</ul>";
-  } else {
-    echo "Ungültige Anfrage";
-  }
-  ?>
-
-  <nav>
-    <form action="index.php" method="get">
-      <button type="submit">Weitere Anmeldung</button>
-    </form>
-    <form action="participants.php" method="get">
-      <button type="submit">Meldeliste anzeigen</button>
-    </form>
-    <form action="unsubscribe.php" method="get">
-      <!-- Ersetzen Sie "abmeldeseite.php" durch die URL Ihrer Abmeldeseite -->
-      <button type="submit">Anmeldung beenden</button>
-    </form>
-  </nav>
-
+    <?php
+    // Navigation einfügen
+    include('navigation.php');
+    ?>
+  </div>
+  
 </body>
 
 </html>
