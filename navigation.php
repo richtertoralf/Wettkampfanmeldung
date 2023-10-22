48f3814e98d5ac23e01cb687a8755ddcee003054
Toralf Richter <73883286+richtertoralf@users.noreply.github.com>
2023-10-23 00:06:07 +0200

    Create navigation.php

---
+++ navigation.php
@@ -0,0 +1,12 @@
+<!-- navigation.php -->
+<nav>
+  <form action="index.php" method="get">
+    <button type="submit">Weitere Anmeldung</button>
+  </form>
+  <form action="participants.php" method="get">
+    <button type="submit">Meldeliste anzeigen</button>
+  </form>
+  <form action="unsubscribe.php" method="get">
+    <button type="submit">Anmeldung beenden</button>
+  </form>
+</nav>
