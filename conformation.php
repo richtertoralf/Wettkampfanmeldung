2604aaeba74785568e0ea68706f641063b51af26
Toralf Richter <73883286+richtertoralf@users.noreply.github.com>
2023-10-23 17:25:36 +0200

    Update conformation.php

--- conformation.php
+++ conformation.php
@@ -1,3 +1,19 @@
+<?php
+/**
+ * conformation.php
+ *
+ * Diese Datei verarbeitet die Formulardaten von 'index.php' nach dem Abschicken des Formulars.
+ * Die Daten werden in einer CSV-Datei gespeichert und auf der Webseite angezeigt.
+ *
+ * PHP-Version 8
+ *
+ * @category   Data Processingg
+ * @package    Registration_Form
+ * @author     Toralf Richter
+ * @link       https://github.com/richtertoralf/sport-registration/
+ */
+?>
+
 <!DOCTYPE html>
 <html lang="de">
 
@@ -13,54 +29,52 @@
   <div class="container_registration">
 
     <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-      // Formulardaten erhalten
-      $spNr = $_POST["regEntry"][0]["SpNr"];
-      $surname = $_POST["regEntry"][0]["surname"];
-      $forename = $_POST["regEntry"][0]["forename"];
-      $sex = $_POST["regEntry"][0]["sex"];
-      $yearofbirth = $_POST["regEntry"][0]["yearofbirth"];
-      $club = $_POST["regEntry"][0]["club"];
-      $group = $_POST["regEntry"][0]["group"];
-      $email = $_POST["single_email"];
+// Überprüfen, ob die Anfrage vom POST-Formular kommt
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Formulardaten erhalten
+    $spNr = $_POST["regEntry"][0]["SpNr"];
+    $surname = $_POST["regEntry"][0]["surname"];
+    $forename = $_POST["regEntry"][0]["forename"];
+    $sex = $_POST["regEntry"][0]["sex"];
+    $yearofbirth = $_POST["regEntry"][0]["yearofbirth"];
+    $club = $_POST["regEntry"][0]["club"];
+    $group = $_POST["regEntry"][0]["group"];
+    $email = $_POST["single_email"];
 
-      // Spaltenköpfe für die CSV-Datei
-      $headers = ["Startpassnummer", "Nachname", "Vorname", "Geschlecht", "Geburtsjahr", "Verein", "Gruppe", "E-Mail"];
+    // Spaltenköpfe für die CSV-Datei
+    $headers = ["Startpassnummer", "Nachname", "Vorname", "Geschlecht", "Geburtsjahr", "Verein", "Gruppe", "E-Mail"];
 
-      // Daten in CSV-Datei speichern
-      $data = [$spNr, $surname, $forename, $sex, $yearofbirth, $club, $group, $email];
-      $csvFile = fopen("data.csv", "a"); // öffne die CSV-Datei im Anhänge-Modus
+    // Daten in CSV-Datei speichern
+    $data = [$spNr, $surname, $forename, $sex, $yearofbirth, $club, $group, $email];
+    $csvFile = fopen("data.csv", "a"); // öffne die CSV-Datei im Anhänge-Modus
 
-      if (filesize("data.csv") == 0) {
+    if (filesize("data.csv") == 0) {
         // Wenn die Datei leer ist, schreibe die Spaltenköpfe
         fputcsv($csvFile, $headers);
-      }
-
-      fputcsv($csvFile, $data); // schreibe die Daten in die CSV-Datei
-      // fwrite($csvFile, PHP_EOL); // füge einen Zeilenumbruch hinzu
-      fclose($csvFile); // schließe die CSV-Datei
-
-      // Daten auf der Webseite anzeigen
-      echo "<h2>Einzelanmeldung erfolgreich eingegangen:</h2>";
-      echo "<ul>";
-      echo "<li><strong>Startpassnummer:</strong> $spNr</li>";
-      echo "<li><strong>Nachname:</strong> $surname</li>";
-      echo "<li><strong>Vorname:</strong> $forename</li>";
-      echo "<li><strong>Geschlecht:</strong> $sex</li>";
-      echo "<li><strong>Geburtsjahr:</strong> $yearofbirth</li>";
-      echo "<li><strong>Verein:</strong> $club</li>";
-      echo "<li><strong>Gruppe:</strong> $group</li>";
-      echo "<li><strong>E-Mail:</strong> $email</li>";
-      echo "</ul>";
-    } else {
-      echo "Ungültige Anfrage";
     }
-    ?>
 
-    <?php
-    // Navigation einfügen
-    include('navigation.php');
-    ?>
+    fputcsv($csvFile, $data); // schreibe die Daten in die CSV-Datei
+    fclose($csvFile); // schließe die CSV-Datei
+
+    // Daten auf der Webseite anzeigen
+    echo "<h2>Einzelanmeldung erfolgreich eingegangen:</h2>";
+    echo "<ul>";
+    echo "<li><strong>Startpassnummer:</strong> $spNr</li>";
+    echo "<li><strong>Nachname:</strong> $surname</li>";
+    echo "<li><strong>Vorname:</strong> $forename</li>";
+    echo "<li><strong>Geschlecht:</strong> $sex</li>";
+    echo "<li><strong>Geburtsjahr:</strong> $yearofbirth</li>";
+    echo "<li><strong>Verein:</strong> $club</li>";
+    echo "<li><strong>Gruppe:</strong> $group</li>";
+    echo "<li><strong>E-Mail:</strong> $email</li>";
+    echo "</ul>";
+} else {
+    echo "Ungültige Anfrage";
+}
+
+// Navigation einfügen
+include('navigation.php');
+?>
   </div>
   
 </body>
