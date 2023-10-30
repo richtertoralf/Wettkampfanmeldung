9b01eb893a8533abdb042332e0b129acf73e5e0f
Toralf Richter <73883286+richtertoralf@users.noreply.github.com>
2023-10-30 21:14:02 +0100

    Update and rename raceInfo.php to php/raceInfo.php

--- php/raceInfo.php
+++ php/raceInfo.php
@@ -1,8 +1,16 @@
 <?php
+
+chdir(__DIR__);
+
 // Lese die Werte aus der INI-Datei
-$raceinfo = parse_ini_file('raceInfo.ini');
+$raceinfo = parse_ini_file('../ini/raceInfo.ini');
 
 // Werte aus der INI-Datei extrahieren
 $eventName = $raceinfo['EventName'];
 $organizer = $raceinfo['Organizer'];
 $date = $raceinfo['Date'];
+
+// Die Werte direkt in HTML ausgeben
+echo "<p>$eventName</p>";
+echo "<p>$organizer</p>";
+echo "<p>$date</p>";
