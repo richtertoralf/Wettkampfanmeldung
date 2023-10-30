67f23507061bb13e93302c606c18becfef201a8d
Toralf Richter <73883286+richtertoralf@users.noreply.github.com>
2023-10-30 21:19:28 +0100

    Update upload.php

--- upload.php
+++ upload.php
@@ -33,6 +33,80 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sammelanmeldung</title>
+    <link rel="icon" type="image/x-icon" href="skiCC.ico">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<!DOCTYPE html>
+<html lang="de">
+
+<body>
+
+    <div class="container registration">
+
+        <header>
+            <?php require('php/raceInfo.php'); ?>
+        </header>
+
+        <h2>Sammelanmeldung</h2>
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+            <input type="file" name="uploadedFile" accept=".csv" required>
+            <button type="submit">Meldedatei hochladen</button>
+            <details>
+                <summary>
+                    Bitte verwende unbedingt diese Vorlage für die Meldedatei: <a href="VorlageMeldedatei.csv" download>VorlageMeldedatei.csv</a>
+                </summary>
+                <p>Die Meldeliste muss den Vorgaben für das Programm Winlaufen des Deutschen Skiverbandes entsprechen.</p>
+                <p>Die Spaltenköpfe dürfen nicht verändert werden.</p>
+            </details>
+        </form>
+
+        <?php
+        // Pfad zur bestehenden CSV-Datei
+        $csvFilePath = 'data/data.csv';
+        // Prüfen, ob das Formular abgeschickt wurde
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['uploadedFile'])) {
+            // Hochgeladene Datei öffnen und ihren Inhalt lesen
+            $uploadedData = file_get_contents($_FILES['uploadedFile']['tmp_name']);
+
+            // Spaltenköpfe der hochgeladenen Datei extrahieren und Leerzeichen entfernen sowie zu Kleinbuchstaben umwandeln
+            $uploadedLines = explode("\n", $uploadedData);
+            $uploadedHeaders = str_getcsv(trim(strtolower(str_replace(' ', '', $uploadedLines[0]))));
+
+            // Spaltenköpfe der bestehenden Datei extrahieren und Leerzeichen entfernen sowie zu Kleinbuchstaben umwandeln
+            $existingData = file_get_contents($csvFilePath);
+            $existingLines = explode("\n", $existingData);
+            $existingHeaders = str_getcsv(trim(strtolower(str_replace(' ', '', $existingLines[0]))));
+
+            // Überprüfen, ob die Spaltenköpfe übereinstimmen
+            if ($uploadedHeaders === $existingHeaders) {
+                // Daten aus der hochgeladenen Datei zur bestehenden CSV-Datei hinzufügen
+                $dataToAdd = implode("\n", array_slice($uploadedLines, 1)); // Nur die Zeilen mit den tatsächlichen Daten
+                if (file_put_contents($csvFilePath, $dataToAdd, FILE_APPEND | LOCK_EX) !== FALSE) {
+                    echo '<aside><p>Die Daten wurden erfolgreich zur Meldeliste hinzugefügt.</p></aside>';
+                } else {
+                    echo '<aside><p>Es gab ein Problem beim Hinzufügen der Daten zur Meldeliste.</p></aside>';
+                }
+            } else {
+                echo '<aside><p>Die Spaltenköpfe der hochgeladenen Datei stimmen nicht mit denen der bestehenden Datei überein.</p></aside>';
+            }
+        }
+
+        // Navigation einfügen
+        require('php/navigation.php');
+        ?>
+    </div>
+
+</body>
+
+</html>
+
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sammelanmeldung</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <!DOCTYPE html>
