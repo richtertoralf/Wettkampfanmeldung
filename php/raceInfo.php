c71e911195d4616286f2fa7c1dfbd86002dbf840
Toralf Richter <73883286+richtertoralf@users.noreply.github.com>
2023-11-12 13:30:06 +0100

    Update raceInfo.php

--- php/raceInfo.php
+++ php/raceInfo.php
@@ -1,16 +1,37 @@
 <?php
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+
+/**
+ * raceInfo.php
+ *
+ * Dieses Skript liest Renninformationen aus einer INI-Datei und gibt sie als assoziatives Array zurück.
+ *
+ * PHP-Version 8
+ *
+ * @category   Data Processing
+ * @package    Participant_Registration
+ * @author     Toralf Richter
+ * @link       https://github.com/richtertoralf/sport-registration/
+ */
 
 chdir(__DIR__);
 
-// Lese die Werte aus der INI-Datei
-$raceinfo = parse_ini_file('../ini/raceInfo.ini');
+/**
+ * Funktion zum Lesen von Renninformationen aus einer INI-Datei.
+ *
+ * @return array - Ein assoziatives Array mit den Renninformationen (EventName, Organizer, Date).
+ */
+function getRaceInfo()
+{
+    // Lese die Werte aus der INI-Datei
+    $raceinfo = parse_ini_file('../data/raceInfo.ini');
+    // var_dump($raceinfo);
+    // Werte aus der INI-Datei extrahieren
+    $eventName = $raceinfo['eventName'];
+    $organizer = $raceinfo['organizer'];
+    $date = $raceinfo['date'];
 
-// Werte aus der INI-Datei extrahieren
-$eventName = $raceinfo['EventName'];
-$organizer = $raceinfo['Organizer'];
-$date = $raceinfo['Date'];
-
-// Die Werte direkt in HTML ausgeben
-echo "<p>$eventName</p>";
-echo "<p>$organizer</p>";
-echo "<p>$date</p>";
+    // Gib ein Array mit den Werten zurück
+    return array('eventName' => $eventName, 'organizer' => $organizer, 'date' => $date);
+}
