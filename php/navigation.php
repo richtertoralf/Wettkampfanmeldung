da4a9e90a64807e5dba81a4e760c4d95d796d0d7
Toralf Richter <73883286+richtertoralf@users.noreply.github.com>
2023-11-12 13:31:23 +0100

    Delete php/navigation.php

--- php/navigation.php
+++
@@ -1,7 +0,0 @@
-<!-- navigation.php -->
-
-<nav>
-  <a href="index.php" class="nav-button">Weitere Anmeldung</a>
-  <a href="participants.php" class="nav-button">Meldeliste anzeigen</a>
-  <a href="unsubscribe.php" class="nav-button">Anmeldung beenden</a>
-</nav>
